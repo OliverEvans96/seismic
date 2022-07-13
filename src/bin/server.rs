13c56b4ab64c94beb15f55b94d8da3e275714723
Oliver Evans <oliverevans96@gmail.com>
2022-07-12 23:03:53 -0700

    CLI opts to server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,22 @@
 use std::{net::SocketAddr, time::Duration};
 
+use clap::Parser;
 use tokio::net::{TcpListener, TcpStream};
 
 use seismic::receiver::{Receiver, ReceiverConfig};
-use seismic::{CONTROL_PORT, DATA_PORT};
 
-async fn listen_control() -> anyhow::Result<()> {
-    let addr = format!("0.0.0.0:{}", CONTROL_PORT);
+#[derive(Parser)]
+struct Opts {
+    /// TCP port for control commands.
+    #[clap(long, default_value = "7224")]
+    control_port: u16,
+    /// TCP port for data transfer.
+    #[clap(long, default_value = "7225")]
+    data_port: u16,
+}
+
+async fn listen_control(port: u16) -> anyhow::Result<()> {
+    let addr = format!("0.0.0.0:{}", port);
     let listener = TcpListener::bind(&addr).await?;
     println!("Listening on control port {}", addr);
 
@@ -21,8 +31,8 @@ async fn handle_control(_stream: TcpStream, addr: SocketAddr) {
     println!("Handling control connection from {}", addr);
 }
 
-async fn listen_data(config: ReceiverConfig) -> anyhow::Result<()> {
-    let addr = format!("0.0.0.0:{}", DATA_PORT);
+async fn listen_data(port: u16, config: ReceiverConfig) -> anyhow::Result<()> {
+    let addr = format!("0.0.0.0:{}", port);
     let listener = TcpListener::bind(&addr).await?;
 
     println!("Listening on data port {}", addr);
@@ -48,17 +58,18 @@ async fn handle_data(stream: TcpStream, addr: SocketAddr, config: ReceiverConfig
 
 #[tokio::main]
 async fn main() {
+    let opts = Opts::parse();
+
     println!("Hello, server!");
 
-    // TODO: Set via config / CLI args
     let config = ReceiverConfig {
         freq: Duration::from_millis(200),
         length: Duration::from_secs(5),
         chunk_size: 1024,
     };
 
-    let data_fut = listen_data(config);
-    let control_fut = listen_control();
+    let data_fut = listen_data(opts.data_port, config);
+    let control_fut = listen_control(opts.control_port);
 
     let (data_res, control_res) = tokio::join!(data_fut, control_fut);
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@ use std::net::IpAddr;
 
 use uuid::Uuid;
 
-pub const CONTROL_PORT: u16 = 7224;
-pub const DATA_PORT: u16 = 7225;
-
 pub struct Node {
     pub id: Uuid,
     // TODO: Multiple IPs per node
