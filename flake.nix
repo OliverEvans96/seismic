21c6541ba65cf000d1e5e8e770ed6378fedca2dd
Oliver Evans <oliverevans96@gmail.com>
2022-07-14 15:55:54 -0700

    Add nix-based docker image, k8s yamls, .gitlab-ci.yml

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         rustPlatform = pkgs.rustPlatform;
-      in {
+      in rec {
         defaultPackage = rustPlatform.buildRustPackage {
           pname = "seismic";
           version = "0.1.0";
@@ -19,6 +19,28 @@
           cargoLock = { lockFile = ./Cargo.lock; };
 
           src = ./.;
+        };
+
+        packages = {
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "seismic-docker";
+            tag = "latest";
+            # Config options reference:
+            # https://github.com/moby/moby/blob/master/image/spec/v1.2.md#image-json-field-descriptions
+            config.Cmd = [ "${defaultPackage}/bin/server" ];
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              pathsToLink = [ "/bin" ];
+              paths = with pkgs; [
+                bash # bash
+                coreutils # ls, cat, etc
+                inetutils # ip, ifconfig, etc.
+                iana-etc # /etc/protocols
+                netcat-gnu # nc
+                defaultPackage # this application
+              ];
+            };
+          };
         };
 
         devShell = pkgs.mkShell {
